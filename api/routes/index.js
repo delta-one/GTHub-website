9e1005a1cc9f2e40ea23a8d57d4b4e0f4bac8ede
Patrick Franz <patfra71@gmail.com>
2019-10-06 02:01:48 +0200

    Disable controllers to not need database for WEC

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const util = require('../util/util.js');
+// const util = require('../util/util.js');
 
 // controllers
-const StartpageCtrl = require('../controllers/startpage');
-const CalendarCtrl = require('../controllers/calendar');
-const AuthCtrl = require('../controllers/auth.js');
-const EventCtrl = require('../controllers/event.js');
-const EventSessionCtrl = require('../controllers/eventsession.js');
-const SeriesCtrl = require('../controllers/series.js');
-const TrackCtrl = require('../controllers/track');
-const GalleryCtrl = require('../controllers/gallery');
-const PodcastCtrl = require('../controllers/podcast');
+// const StartpageCtrl = require('../controllers/startpage');
+// const CalendarCtrl = require('../controllers/calendar');
+// const AuthCtrl = require('../controllers/auth.js');
+// const EventCtrl = require('../controllers/event.js');
+// const EventSessionCtrl = require('../controllers/eventsession.js');
+// const SeriesCtrl = require('../controllers/series.js');
+// const TrackCtrl = require('../controllers/track');
+// const GalleryCtrl = require('../controllers/gallery');
+// const PodcastCtrl = require('../controllers/podcast');
 const WecCtrl = require('../controllers/wec.js');
 
 
@@ -22,29 +22,29 @@ const WecCtrl = require('../controllers/wec.js');
 router.get('/wecdata', WecCtrl.getWecData);
 
 // Authentication
-router.post('/login', AuthCtrl.login);
-router.post('/logout', AuthCtrl.logout);
+// router.post('/login', AuthCtrl.login);
+// router.post('/logout', AuthCtrl.logout);
 // router.post('/changepassword', AuthCtrl.changepassword);
 
 // Calendar
-router.get('/calendar', CalendarCtrl.getCalendar);
+// router.get('/calendar', CalendarCtrl.getCalendar);
 // router.post('/calendar/timezone', CalendarCtrl.getCalendarWithTimezone);
 
 // Event
-router.post('/calendar/event/create', EventCtrl.createEvent);
-router.post('/calendar/event/update/:id', EventCtrl.updateEvent);
-router.post('/calendar/event/delete/:id', EventCtrl.deleteEvent);
+// router.post('/calendar/event/create', EventCtrl.createEvent);
+// router.post('/calendar/event/update/:id', EventCtrl.updateEvent);
+// router.post('/calendar/event/delete/:id', EventCtrl.deleteEvent);
 
 // EventSession
-router.post('/calendar/eventsession/create', EventSessionCtrl.createEventSession);
-router.post('/calendar/eventsession/update/:id', EventSessionCtrl.updateEventSession);
-router.post('/calendar/eventsession/delete/:id', EventSessionCtrl.deleteEventSession);
+// router.post('/calendar/eventsession/create', EventSessionCtrl.createEventSession);
+// router.post('/calendar/eventsession/update/:id', EventSessionCtrl.updateEventSession);
+// router.post('/calendar/eventsession/delete/:id', EventSessionCtrl.deleteEventSession);
 
 // Series
-router.post('/calendar/series/create', SeriesCtrl.createSeries);
+// router.post('/calendar/series/create', SeriesCtrl.createSeries);
 
 // Track
-router.post('/calendar/track/create', TrackCtrl.createTrack);
+// router.post('/calendar/track/create', TrackCtrl.createTrack);
 //
 // // Gallery
 // router.get('/gallery', GalleryCtrl.getGallery);
